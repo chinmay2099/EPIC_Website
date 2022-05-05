3b3f36e702c09449ba3e82ce28be987dbeab14b9
Manish Rajendra Patil <mrajendr@tibco.com>
2022-05-05 08:40:26 +0530

    first commit

---
+++ QUIZ/js/questions.js
@@ -0,0 +1,113 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What is the name of the biggest technology company in South Korea?",
+    answer: "Samsung",
+    options: [
+      "Oppo",
+      "LG",
+      "Samsung",
+      "Xiaomi"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What was the first soft drink in space?",
+    answer: "Coca Cola",
+    options: [
+      "Coca Cola",
+      "Thumps Up",
+      "Fanta",
+      "Slice"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which country invented ice cream?",
+    answer: "China",
+    options: [
+      "Canada",
+      "US",
+      "India",
+      "China"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Dump,floater and wipe are terms used in which team sort?",
+    answer: "Volleyball",
+    options: [
+      "Cricket",
+      "Volleyball",
+      "Football",
+      "Tennis"
+    ]
+  },
+    {
+    numb: 5,
+    question: "How many points did michael Jordan scores on his first NBA game?",
+    answer: "16 Points",
+    options: [
+      "16 points",
+      "20 points",
+      "18 points",
+      "15 points"
+    ]
+  },
+  {
+    numb: 6,
+    question: "What country has the most natural lakes?",
+    answer: "Canada",
+    options: [
+      "America",
+      "Canada",
+      "Australia",
+      "Paris"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Which is the only edible food that never goes bad?",
+    answer: "Honey",
+    options: [
+      "Ice Cream",
+      "Milk",
+      "Sugar",
+      "Honey"
+    ]
+  },
+  {
+    numb: 8,
+    question: "What famous US festival hosted over 350,000 fans in 1969?",
+    answer: "Woodstalk",
+    options: [
+      "Notting Hill Carnival",
+      "Robin Hood Festival ",
+      "Woodstalk",
+      "Diwali"
+    ]
+  },
+  {
+    numb: 9,
+    question: "FIFA is the global regulatory body of which sports ?",
+    answer: "Football",
+    options: [
+      "Tennis",
+      "Football ",
+      "Cricket",
+      "Hockey"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Who was the first indian woman who get an Olympic Medal?",
+    answer: "Football",
+    options: [
+      "P.T.Usha",
+      "Sania Mirza ",
+      "Aarti Saha",
+      "Karnam Malleswari"
+    ]
+  },
+];
